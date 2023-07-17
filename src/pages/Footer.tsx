a34a7566645bd704271417724906fa3e5e109466
emonmahdi <developeremon1997@gmail.com>
2023-07-17 18:26:55 +0600

    create footer design

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,4 +1,4 @@
-/* import React from "react";
+import React from "react";
 
 export default function Footer() {
   return (
@@ -35,4 +35,3 @@ export default function Footer() {
     </div>
   );
 }
- */
